1b4e71e52f524c65b4ba57e140ad6d770d2ac263
carlosperea14 <carlos.perea@inacapmail.cl>
2024-12-03 11:16:33 -0300

    se realizaron forms

--- SimpleFacturaSDK-Demo/ConsolidadoVenta.Designer.cs
+++ SimpleFacturaSDK-Demo/ConsolidadoVenta.Designer.cs
@@ -32,35 +32,34 @@
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.radioProduccion = new System.Windows.Forms.RadioButton();
             this.radioCertificacion = new System.Windows.Forms.RadioButton();
-            this.tipodte_oXML = new System.Windows.Forms.ComboBox();
-            this.folio_oPDF = new System.Windows.Forms.NumericUpDown();
             this.textBox5 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.hasta = new System.Windows.Forms.Label();
+            this.ambiente = new System.Windows.Forms.Label();
+            this.desde = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.textRutEmisor = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.cancelarXml = new System.Windows.Forms.Button();
             this.generarpdf = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.folio_oPDF)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.dateTimePicker2);
+            this.groupBox2.Controls.Add(this.dateTimePicker1);
             this.groupBox2.Controls.Add(this.radioProduccion);
             this.groupBox2.Controls.Add(this.radioCertificacion);
-            this.groupBox2.Controls.Add(this.tipodte_oXML);
-            this.groupBox2.Controls.Add(this.folio_oPDF);
             this.groupBox2.Controls.Add(this.textBox5);
-            this.groupBox2.Controls.Add(this.label5);
-            this.groupBox2.Controls.Add(this.label3);
-            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.hasta);
+            this.groupBox2.Controls.Add(this.ambiente);
+            this.groupBox2.Controls.Add(this.desde);
             this.groupBox2.Location = new System.Drawing.Point(12, 96);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(501, 162);
+            this.groupBox2.Size = new System.Drawing.Size(330, 162);
             this.groupBox2.TabIndex = 13;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Otros";
@@ -68,7 +67,7 @@
             // radioProduccion
             // 
             this.radioProduccion.AutoSize = true;
-            this.radioProduccion.Location = new System.Drawing.Point(245, 93);
+            this.radioProduccion.Location = new System.Drawing.Point(220, 105);
             this.radioProduccion.Name = "radioProduccion";
             this.radioProduccion.Size = new System.Drawing.Size(96, 20);
             this.radioProduccion.TabIndex = 17;
@@ -79,33 +78,13 @@
             // radioCertificacion
             // 
             this.radioCertificacion.AutoSize = true;
-            this.radioCertificacion.Location = new System.Drawing.Point(125, 93);
+            this.radioCertificacion.Location = new System.Drawing.Point(97, 105);
             this.radioCertificacion.Name = "radioCertificacion";
             this.radioCertificacion.Size = new System.Drawing.Size(101, 20);
             this.radioCertificacion.TabIndex = 16;
             this.radioCertificacion.TabStop = true;
             this.radioCertificacion.Text = "Certificacion";
             this.radioCertificacion.UseVisualStyleBackColor = true;
-            // 
-            // tipodte_oXML
-            // 
-            this.tipodte_oXML.FormattingEnabled = true;
-            this.tipodte_oXML.Location = new System.Drawing.Point(123, 54);
-            this.tipodte_oXML.Name = "tipodte_oXML";
-            this.tipodte_oXML.Size = new System.Drawing.Size(187, 24);
-            this.tipodte_oXML.TabIndex = 15;
-            // 
-            // folio_oPDF
-            // 
-            this.folio_oPDF.Location = new System.Drawing.Point(124, 22);
-            this.folio_oPDF.Name = "folio_oPDF";
-            this.folio_oPDF.Size = new System.Drawing.Size(186, 22);
-            this.folio_oPDF.TabIndex = 14;
-            this.folio_oPDF.Value = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
             // 
             // textBox5
             // 
@@ -114,32 +93,32 @@
             this.textBox5.Size = new System.Drawing.Size(413, 22);
             this.textBox5.TabIndex = 9;
             // 
-            // label5
+            // hasta
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 58);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(113, 16);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Codigo TipoDTE:";
+            this.hasta.AutoSize = true;
+            this.hasta.Location = new System.Drawing.Point(6, 70);
+            this.hasta.Name = "hasta";
+            this.hasta.Size = new System.Drawing.Size(46, 16);
+            this.hasta.TabIndex = 8;
+            this.hasta.Text = "Hasta:";
             // 
-            // label3
+            // ambiente
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 93);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 16);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Ambiente:";
+            this.ambiente.AutoSize = true;
+            this.ambiente.Location = new System.Drawing.Point(6, 105);
+            this.ambiente.Name = "ambiente";
+            this.ambiente.Size = new System.Drawing.Size(67, 16);
+            this.ambiente.TabIndex = 5;
+            this.ambiente.Text = "Ambiente:";
             // 
-            // label4
+            // desde
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 22);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(40, 16);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Folio:";
+            this.desde.AutoSize = true;
+            this.desde.Location = new System.Drawing.Point(6, 34);
+            this.desde.Name = "desde";
+            this.desde.Size = new System.Drawing.Size(51, 16);
+            this.desde.TabIndex = 4;
+            this.desde.Text = "Desde:";
             // 
             // groupBox1
             // 
@@ -147,22 +126,22 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(462, 78);
+            this.groupBox1.Size = new System.Drawing.Size(330, 78);
             this.groupBox1.TabIndex = 12;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Credenciales";
             // 
             // textRutEmisor
             // 
-            this.textRutEmisor.Location = new System.Drawing.Point(87, 25);
+            this.textRutEmisor.Location = new System.Drawing.Point(100, 29);
             this.textRutEmisor.Name = "textRutEmisor";
-            this.textRutEmisor.Size = new System.Drawing.Size(351, 22);
+            this.textRutEmisor.Size = new System.Drawing.Size(216, 22);
             this.textRutEmisor.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 28);
+            this.label1.Location = new System.Drawing.Point(6, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(75, 16);
             this.label1.TabIndex = 0;
@@ -173,7 +152,7 @@
             this.cancelarXml.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cancelarXml.Image = global::SimpleFacturaSDK_Demo.Properties.Resources.cruzar;
             this.cancelarXml.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.cancelarXml.Location = new System.Drawing.Point(303, 269);
+            this.cancelarXml.Location = new System.Drawing.Point(127, 269);
             this.cancelarXml.Name = "cancelarXml";
             this.cancelarXml.Padding = new System.Windows.Forms.Padding(2, 2, 0, 0);
             this.cancelarXml.Size = new System.Drawing.Size(102, 35);
@@ -187,7 +166,7 @@
             this.generarpdf.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.generarpdf.Image = global::SimpleFacturaSDK_Demo.Properties.Resources.Guardar_32;
             this.generarpdf.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.generarpdf.Location = new System.Drawing.Point(411, 269);
+            this.generarpdf.Location = new System.Drawing.Point(235, 269);
             this.generarpdf.Name = "generarpdf";
             this.generarpdf.Padding = new System.Windows.Forms.Padding(2, 2, 0, 0);
             this.generarpdf.Size = new System.Drawing.Size(102, 35);
@@ -196,11 +175,27 @@
             this.generarpdf.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.generarpdf.UseVisualStyleBackColor = true;
             // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(98, 33);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(130, 22);
+            this.dateTimePicker1.TabIndex = 18;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker2.Location = new System.Drawing.Point(98, 68);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(132, 22);
+            this.dateTimePicker2.TabIndex = 19;
+            // 
             // ConsolidadoVenta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(360, 319);
             this.Controls.Add(this.cancelarXml);
             this.Controls.Add(this.generarpdf);
             this.Controls.Add(this.groupBox2);
@@ -213,7 +208,6 @@
             this.Text = "Consolidado de Ventas";
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.folio_oPDF)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -227,14 +221,14 @@
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.RadioButton radioProduccion;
         private System.Windows.Forms.RadioButton radioCertificacion;
-        private System.Windows.Forms.ComboBox tipodte_oXML;
-        private System.Windows.Forms.NumericUpDown folio_oPDF;
         private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label hasta;
+        private System.Windows.Forms.Label ambiente;
+        private System.Windows.Forms.Label desde;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox textRutEmisor;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
