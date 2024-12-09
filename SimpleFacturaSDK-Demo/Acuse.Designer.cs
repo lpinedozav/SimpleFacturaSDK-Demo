993542980dcb7a5ef87dcb6c626695cfc32f1d6c
carlosperea14 <carlos.perea@inacapmail.cl>
2024-12-09 10:57:54 -0300

    se hizo el formulario de listado Dte Recibidos

--- SimpleFacturaSDK-Demo/Acuse.Designer.cs
+++ SimpleFacturaSDK-Demo/Acuse.Designer.cs
@@ -293,8 +293,12 @@
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Acuse";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Acuse";
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
