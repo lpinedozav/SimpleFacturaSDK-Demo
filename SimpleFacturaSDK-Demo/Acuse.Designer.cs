7e69347169e02ebe306024c7e95f6cc0aa43222c
carlosperea14 <carlos.perea@inacapmail.cl>
2024-12-09 10:39:03 -0300

    se hizo el formulario de acuse

---
+++ SimpleFacturaSDK-Demo/Acuse.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace SimpleFacturaSDK_Demo
+{
+    partial class Acuse
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Acuse));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tipodte_oPDF = new System.Windows.Forms.ComboBox();
+            this.folio_oPDF = new System.Windows.Forms.NumericUpDown();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textNombreSucursal = new System.Windows.Forms.TextBox();
+            this.textRutEmisor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.comboBoxRespuesta = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TextComentario = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.generarpdf = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.folio_oPDF)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tipodte_oPDF);
+            this.groupBox2.Controls.Add(this.folio_oPDF);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(11, 98);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(242, 75);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Dte Referenciado Externo";
+            // 
+            // tipodte_oPDF
+            // 
+            this.tipodte_oPDF.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.tipodte_oPDF.FormattingEnabled = true;
+            this.tipodte_oPDF.Location = new System.Drawing.Point(92, 44);
+            this.tipodte_oPDF.Margin = new System.Windows.Forms.Padding(2);
+            this.tipodte_oPDF.Name = "tipodte_oPDF";
+            this.tipodte_oPDF.Size = new System.Drawing.Size(141, 21);
+            this.tipodte_oPDF.TabIndex = 0;
+            this.tipodte_oPDF.TabStop = false;
+            // 
+            // folio_oPDF
+            // 
+            this.folio_oPDF.Location = new System.Drawing.Point(93, 18);
+            this.folio_oPDF.Margin = new System.Windows.Forms.Padding(2);
+            this.folio_oPDF.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.folio_oPDF.Name = "folio_oPDF";
+            this.folio_oPDF.Size = new System.Drawing.Size(140, 20);
+            this.folio_oPDF.TabIndex = 0;
+            this.folio_oPDF.TabStop = false;
+            this.folio_oPDF.Value = new decimal(new int[] {
+            4117,
+            0,
+            0,
+            0});
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(7, 132);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(311, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 47);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Codigo TipoDTE:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 22);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Folio:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textNombreSucursal);
+            this.groupBox1.Controls.Add(this.textRutEmisor);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(11, 11);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(242, 83);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Credenciales";
+            // 
+            // textNombreSucursal
+            // 
+            this.textNombreSucursal.Location = new System.Drawing.Point(93, 50);
+            this.textNombreSucursal.Margin = new System.Windows.Forms.Padding(2);
+            this.textNombreSucursal.Name = "textNombreSucursal";
+            this.textNombreSucursal.Size = new System.Drawing.Size(140, 20);
+            this.textNombreSucursal.TabIndex = 0;
+            this.textNombreSucursal.TabStop = false;
+            // 
+            // textRutEmisor
+            // 
+            this.textRutEmisor.Location = new System.Drawing.Point(93, 23);
+            this.textRutEmisor.Margin = new System.Windows.Forms.Padding(2);
+            this.textRutEmisor.Name = "textRutEmisor";
+            this.textRutEmisor.Size = new System.Drawing.Size(140, 20);
+            this.textRutEmisor.TabIndex = 0;
+            this.textRutEmisor.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 55);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre sucursal:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rut Emisor:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.TextComentario);
+            this.groupBox3.Controls.Add(this.comboBoxRespuesta);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.comboBox1);
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Location = new System.Drawing.Point(11, 177);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Size = new System.Drawing.Size(242, 102);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Otros";
+            // 
+            // comboBoxRespuesta
+            // 
+            this.comboBoxRespuesta.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxRespuesta.FormattingEnabled = true;
+            this.comboBoxRespuesta.Location = new System.Drawing.Point(92, 16);
+            this.comboBoxRespuesta.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxRespuesta.Name = "comboBoxRespuesta";
+            this.comboBoxRespuesta.Size = new System.Drawing.Size(141, 21);
+            this.comboBoxRespuesta.TabIndex = 10;
+            this.comboBoxRespuesta.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(92, 44);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(141, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 132);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(311, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 47);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Tipo Rechazo:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 22);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Respuesta:";
+            // 
+            // TextComentario
+            // 
+            this.TextComentario.Location = new System.Drawing.Point(93, 70);
+            this.TextComentario.Margin = new System.Windows.Forms.Padding(2);
+            this.TextComentario.Name = "TextComentario";
+            this.TextComentario.Size = new System.Drawing.Size(140, 20);
+            this.TextComentario.TabIndex = 2;
+            this.TextComentario.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(4, 75);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 13);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Comentario:";
+            // 
+            // generarpdf
+            // 
+            this.generarpdf.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generarpdf.Image = global::SimpleFacturaSDK_Demo.Properties.Resources.Guardar_32;
+            this.generarpdf.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.generarpdf.Location = new System.Drawing.Point(177, 283);
+            this.generarpdf.Margin = new System.Windows.Forms.Padding(2);
+            this.generarpdf.Name = "generarpdf";
+            this.generarpdf.Padding = new System.Windows.Forms.Padding(2, 2, 0, 0);
+            this.generarpdf.Size = new System.Drawing.Size(76, 28);
+            this.generarpdf.TabIndex = 11;
+            this.generarpdf.Text = "Generar";
+            this.generarpdf.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.generarpdf.UseVisualStyleBackColor = true;
+            // 
+            // Acuse
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 318);
+            this.Controls.Add(this.generarpdf);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Acuse";
+            this.Text = "Acuse";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.folio_oPDF)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox tipodte_oPDF;
+        private System.Windows.Forms.NumericUpDown folio_oPDF;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textNombreSucursal;
+        private System.Windows.Forms.TextBox textRutEmisor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBoxRespuesta;
+        private System.Windows.Forms.TextBox TextComentario;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button generarpdf;
+    }
+}
