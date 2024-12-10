d7ba3248da2153c0250b371547c1a7b565003585
Luis Pinedo <luis.pinedo@inacapmail.cl>
2024-12-10 12:19:28 -0300

    se incorporo listado de productos(falta impuestos)

--- SimpleFacturaSDK-Demo/ListarProductos.Designer.cs
+++ SimpleFacturaSDK-Demo/ListarProductos.Designer.cs
@@ -37,11 +37,11 @@
             this.textRutEmisor = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.generarAgregarProducto = new System.Windows.Forms.Button();
-            this.productoId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.generarListadoProductos = new System.Windows.Forms.Button();
             this.gridNombreProducto = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.gridPrecio = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.exento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.impuesto = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gridResultados)).BeginInit();
             this.groupBox1.SuspendLayout();
@@ -50,9 +50,11 @@
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.gridResultados);
-            this.groupBox3.Location = new System.Drawing.Point(257, 11);
+            this.groupBox3.Location = new System.Drawing.Point(15, 165);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(550, 102);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Size = new System.Drawing.Size(558, 352);
             this.groupBox3.TabIndex = 23;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Productos";
@@ -66,15 +68,16 @@
             this.gridResultados.ColumnHeadersHeight = 29;
             this.gridResultados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
             this.gridResultados.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.productoId,
             this.gridNombreProducto,
             this.gridPrecio,
-            this.exento});
-            this.gridResultados.Location = new System.Drawing.Point(5, 13);
+            this.exento,
+            this.impuesto});
+            this.gridResultados.Location = new System.Drawing.Point(7, 16);
+            this.gridResultados.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gridResultados.Name = "gridResultados";
             this.gridResultados.ReadOnly = true;
             this.gridResultados.RowHeadersWidth = 25;
-            this.gridResultados.Size = new System.Drawing.Size(537, 78);
+            this.gridResultados.Size = new System.Drawing.Size(543, 328);
             this.gridResultados.TabIndex = 0;
             // 
             // groupBox1
@@ -83,83 +86,76 @@
             this.groupBox1.Controls.Add(this.textRutEmisor);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(11, 11);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Location = new System.Drawing.Point(15, 14);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Size = new System.Drawing.Size(241, 102);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(550, 126);
             this.groupBox1.TabIndex = 22;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Credenciales";
             // 
             // textNombreSucursal
             // 
-            this.textNombreSucursal.Location = new System.Drawing.Point(93, 55);
-            this.textNombreSucursal.Margin = new System.Windows.Forms.Padding(2);
+            this.textNombreSucursal.Location = new System.Drawing.Point(124, 68);
+            this.textNombreSucursal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textNombreSucursal.Name = "textNombreSucursal";
-            this.textNombreSucursal.Size = new System.Drawing.Size(140, 20);
+            this.textNombreSucursal.Size = new System.Drawing.Size(420, 22);
             this.textNombreSucursal.TabIndex = 0;
             this.textNombreSucursal.TabStop = false;
             // 
             // textRutEmisor
             // 
-            this.textRutEmisor.Location = new System.Drawing.Point(93, 23);
-            this.textRutEmisor.Margin = new System.Windows.Forms.Padding(2);
+            this.textRutEmisor.Location = new System.Drawing.Point(124, 28);
+            this.textRutEmisor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textRutEmisor.Name = "textRutEmisor";
-            this.textRutEmisor.Size = new System.Drawing.Size(140, 20);
+            this.textRutEmisor.Size = new System.Drawing.Size(420, 22);
             this.textRutEmisor.TabIndex = 0;
             this.textRutEmisor.TabStop = false;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(4, 59);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(5, 73);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.Size = new System.Drawing.Size(112, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Nombre sucursal:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(4, 23);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(5, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.Size = new System.Drawing.Size(75, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Rut Emisor:";
             // 
-            // generarAgregarProducto
+            // generarListadoProductos
             // 
-            this.generarAgregarProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.generarAgregarProducto.Image = global::SimpleFacturaSDK_Demo.Properties.Resources.Guardar_32;
-            this.generarAgregarProducto.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.generarAgregarProducto.Location = new System.Drawing.Point(731, 118);
-            this.generarAgregarProducto.Margin = new System.Windows.Forms.Padding(2);
-            this.generarAgregarProducto.Name = "generarAgregarProducto";
-            this.generarAgregarProducto.Padding = new System.Windows.Forms.Padding(2, 2, 0, 0);
-            this.generarAgregarProducto.Size = new System.Drawing.Size(76, 28);
-            this.generarAgregarProducto.TabIndex = 24;
-            this.generarAgregarProducto.Text = "Generar";
-            this.generarAgregarProducto.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.generarAgregarProducto.UseVisualStyleBackColor = true;
-            // 
-            // productoId
-            // 
-            this.productoId.HeaderText = "Producto Id";
-            this.productoId.Name = "productoId";
-            this.productoId.ReadOnly = true;
-            this.productoId.Width = 187;
+            this.generarListadoProductos.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generarListadoProductos.Image = global::SimpleFacturaSDK_Demo.Properties.Resources.Guardar_32;
+            this.generarListadoProductos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.generarListadoProductos.Location = new System.Drawing.Point(472, 523);
+            this.generarListadoProductos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.generarListadoProductos.Name = "generarListadoProductos";
+            this.generarListadoProductos.Padding = new System.Windows.Forms.Padding(3, 2, 0, 0);
+            this.generarListadoProductos.Size = new System.Drawing.Size(101, 34);
+            this.generarListadoProductos.TabIndex = 24;
+            this.generarListadoProductos.Text = "Generar";
+            this.generarListadoProductos.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.generarListadoProductos.UseVisualStyleBackColor = true;
+            this.generarListadoProductos.Click += new System.EventHandler(this.generarListadoProductos_Click);
             // 
             // gridNombreProducto
             // 
             this.gridNombreProducto.DataPropertyName = "Nombre";
             this.gridNombreProducto.HeaderText = "Nombre";
+            this.gridNombreProducto.MaxInputLength = 20000;
             this.gridNombreProducto.MinimumWidth = 6;
             this.gridNombreProducto.Name = "gridNombreProducto";
             this.gridNombreProducto.ReadOnly = true;
-            this.gridNombreProducto.Width = 187;
+            this.gridNombreProducto.Width = 120;
             // 
             // gridPrecio
             // 
@@ -181,21 +177,31 @@
             this.exento.ReadOnly = true;
             this.exento.Width = 50;
             // 
+            // impuesto
+            // 
+            this.impuesto.HeaderText = "Impuestos";
+            this.impuesto.MinimumWidth = 6;
+            this.impuesto.Name = "impuesto";
+            this.impuesto.ReadOnly = true;
+            this.impuesto.Width = 187;
+            // 
             // ListarProductos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(819, 152);
-            this.Controls.Add(this.generarAgregarProducto);
+            this.ClientSize = new System.Drawing.Size(583, 566);
+            this.Controls.Add(this.generarListadoProductos);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ListarProductos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ListarProductos";
+            this.Load += new System.EventHandler(this.ListarProductos_Load);
             this.groupBox3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.gridResultados)).EndInit();
             this.groupBox1.ResumeLayout(false);
@@ -206,7 +212,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button generarAgregarProducto;
+        private System.Windows.Forms.Button generarListadoProductos;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.DataGridView gridResultados;
         private System.Windows.Forms.GroupBox groupBox1;
@@ -214,9 +220,9 @@
         private System.Windows.Forms.TextBox textRutEmisor;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn productoId;
         private System.Windows.Forms.DataGridViewTextBoxColumn gridNombreProducto;
         private System.Windows.Forms.DataGridViewTextBoxColumn gridPrecio;
         private System.Windows.Forms.DataGridViewTextBoxColumn exento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn impuesto;
     }
 }
--- SimpleFacturaSDK-Demo/ListarProductos.cs
+++ SimpleFacturaSDK-Demo/ListarProductos.cs
@@ -1,20 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SDKSimpleFactura;
+using SDKSimpleFactura.Models.Facturacion;
+using SDKSimpleFactura.Helpers;
+using System;
 using System.Windows.Forms;
+using SimpleFacturaSDK_Demo.Helpers;
 
 namespace SimpleFacturaSDK_Demo
 {
     public partial class ListarProductos : Form
     {
+        private AppSettings _appSettings;
+        private SimpleFacturaClient cliente;
         public ListarProductos()
         {
+            _appSettings = AppSettings.Current;
+            cliente = SimpleClientSingleton.Instance;
             InitializeComponent();
+        }
+
+        private void ListarProductos_Load(object sender, EventArgs e)
+        {
+            textRutEmisor.Text = _appSettings.Credenciales.RutEmisor;
+            textNombreSucursal.Text = _appSettings.Credenciales.NombreSucursal;
+        }
+
+        private async void generarListadoProductos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var request = new Credenciales();
+                request.RutEmisor = textRutEmisor.Text;
+                request.NombreSucursal = textNombreSucursal.Text;
+                var response = await cliente.Productos.ListarProductosAsync(request);
+                if (response.Status == 400 || response.Status == 500)
+                {
+                    MessageBox.Show(response.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    var list = response.Data;
+                    foreach (var producto in list)
+                    {
+                        int rowIndex = gridResultados.Rows.Add(
+                            //producto.ProductoId,
+                            producto.Nombre,
+                            FormattingHelper.FormatearPrecio((decimal)producto.Precio),
+                            producto.Exento
+                        );
+                        //DataGridViewRow row = gridResultados.Rows[rowIndex];
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocurrió un error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
