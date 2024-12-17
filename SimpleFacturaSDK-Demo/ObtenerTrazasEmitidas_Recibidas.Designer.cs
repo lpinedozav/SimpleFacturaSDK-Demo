9ebc32f0693d868007d62329d4da49e9cff878f9
lpinedozavala <lpinedo@chilesystems.com>
2024-12-17 17:46:28 -0300

    Se completo formulario para obtener trazas emitidas y recibidas

--- SimpleFacturaSDK-Demo/ObtenerTrazasEmitidas_Recibidas.Designer.cs
+++ SimpleFacturaSDK-Demo/ObtenerTrazasEmitidas_Recibidas.Designer.cs
@@ -28,7 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ObtenerTrazasEmitidas_Recibidas));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textRutContribuyente = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.textRutEmisor = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
@@ -39,24 +42,26 @@
             this.folio_oPDF = new System.Windows.Forms.NumericUpDown();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.obtener_trazas = new System.Windows.Forms.Button();
-            this.radioButton_recibidoPdf = new System.Windows.Forms.RadioButton();
-            this.radioButton_emitidoPdf = new System.Windows.Forms.RadioButton();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.radioButton_recibido = new System.Windows.Forms.RadioButton();
+            this.radioButton_emitido = new System.Windows.Forms.RadioButton();
+            this.gridTrazas = new System.Windows.Forms.DataGridView();
             this.fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.textDocumentacion = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.folio_oPDF)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTrazas)).BeginInit();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textRutContribuyente);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.textRutEmisor);
             this.groupBox1.Controls.Add(this.label1);
@@ -69,12 +74,31 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Credenciales";
             // 
+            // textRutContribuyente
+            // 
+            this.textRutContribuyente.Location = new System.Drawing.Point(100, 48);
+            this.textRutContribuyente.Margin = new System.Windows.Forms.Padding(2);
+            this.textRutContribuyente.Name = "textRutContribuyente";
+            this.textRutContribuyente.Size = new System.Drawing.Size(265, 20);
+            this.textRutContribuyente.TabIndex = 4;
+            this.textRutContribuyente.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 51);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Rut contribuyente:";
+            // 
             // textRutEmisor
             // 
-            this.textRutEmisor.Location = new System.Drawing.Point(92, 20);
+            this.textRutEmisor.Location = new System.Drawing.Point(100, 20);
             this.textRutEmisor.Margin = new System.Windows.Forms.Padding(2);
             this.textRutEmisor.Name = "textRutEmisor";
-            this.textRutEmisor.Size = new System.Drawing.Size(264, 20);
+            this.textRutEmisor.Size = new System.Drawing.Size(265, 20);
             this.textRutEmisor.TabIndex = 2;
             this.textRutEmisor.TabStop = false;
             // 
@@ -137,14 +161,13 @@
             this.radioCertificacion.Name = "radioCertificacion";
             this.radioCertificacion.Size = new System.Drawing.Size(83, 17);
             this.radioCertificacion.TabIndex = 16;
-            this.radioCertificacion.TabStop = true;
             this.radioCertificacion.Text = "Certificación";
             this.radioCertificacion.UseVisualStyleBackColor = true;
             // 
             // comboBox_codigoTipoDTE
             // 
             this.comboBox_codigoTipoDTE.FormattingEnabled = true;
-            this.comboBox_codigoTipoDTE.Location = new System.Drawing.Point(92, 44);
+            this.comboBox_codigoTipoDTE.Location = new System.Drawing.Point(100, 44);
             this.comboBox_codigoTipoDTE.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox_codigoTipoDTE.Name = "comboBox_codigoTipoDTE";
             this.comboBox_codigoTipoDTE.Size = new System.Drawing.Size(265, 21);
@@ -153,7 +176,7 @@
             // 
             // folio_oPDF
             // 
-            this.folio_oPDF.Location = new System.Drawing.Point(93, 18);
+            this.folio_oPDF.Location = new System.Drawing.Point(100, 18);
             this.folio_oPDF.Margin = new System.Windows.Forms.Padding(2);
             this.folio_oPDF.Maximum = new decimal(new int[] {
             100000,
@@ -161,7 +184,7 @@
             0,
             0});
             this.folio_oPDF.Name = "folio_oPDF";
-            this.folio_oPDF.Size = new System.Drawing.Size(263, 20);
+            this.folio_oPDF.Size = new System.Drawing.Size(265, 20);
             this.folio_oPDF.TabIndex = 14;
             this.folio_oPDF.TabStop = false;
             this.folio_oPDF.Value = new decimal(new int[] {
@@ -190,73 +213,57 @@
             this.label4.TabIndex = 4;
             this.label4.Text = "Folio:";
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(93, 48);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(263, 20);
-            this.textBox1.TabIndex = 4;
-            this.textBox1.TabStop = false;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(4, 51);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Rut Emisor:";
-            // 
             // obtener_trazas
             // 
             this.obtener_trazas.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.obtener_trazas.Image = global::SimpleFacturaSDK_Demo.Properties.Resources.Guardar_32;
             this.obtener_trazas.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.obtener_trazas.Location = new System.Drawing.Point(314, 538);
+            this.obtener_trazas.Location = new System.Drawing.Point(314, 588);
             this.obtener_trazas.Margin = new System.Windows.Forms.Padding(2);
             this.obtener_trazas.Name = "obtener_trazas";
             this.obtener_trazas.Padding = new System.Windows.Forms.Padding(2, 2, 0, 0);
             this.obtener_trazas.Size = new System.Drawing.Size(75, 35);
-            this.obtener_trazas.TabIndex = 19;
+            this.obtener_trazas.TabIndex = 1;
             this.obtener_trazas.Text = "Obtener";
             this.obtener_trazas.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.obtener_trazas.UseVisualStyleBackColor = true;
+            this.obtener_trazas.Click += new System.EventHandler(this.obtener_trazas_Click);
             // 
-            // radioButton_recibidoPdf
+            // radioButton_recibido
             // 
-            this.radioButton_recibidoPdf.Location = new System.Drawing.Point(93, 11);
-            this.radioButton_recibidoPdf.Margin = new System.Windows.Forms.Padding(2);
-            this.radioButton_recibidoPdf.Name = "radioButton_recibidoPdf";
-            this.radioButton_recibidoPdf.Size = new System.Drawing.Size(69, 15);
-            this.radioButton_recibidoPdf.TabIndex = 41;
-            this.radioButton_recibidoPdf.Text = "Recibido";
-            this.radioButton_recibidoPdf.UseVisualStyleBackColor = true;
+            this.radioButton_recibido.Location = new System.Drawing.Point(93, 11);
+            this.radioButton_recibido.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton_recibido.Name = "radioButton_recibido";
+            this.radioButton_recibido.Size = new System.Drawing.Size(69, 15);
+            this.radioButton_recibido.TabIndex = 41;
+            this.radioButton_recibido.Text = "Recibido";
+            this.radioButton_recibido.UseVisualStyleBackColor = true;
+            this.radioButton_recibido.CheckedChanged += new System.EventHandler(this.radioButton_recibido_CheckedChanged);
             // 
-            // radioButton_emitidoPdf
+            // radioButton_emitido
             // 
-            this.radioButton_emitidoPdf.Checked = true;
-            this.radioButton_emitidoPdf.Location = new System.Drawing.Point(18, 11);
-            this.radioButton_emitidoPdf.Margin = new System.Windows.Forms.Padding(2);
-            this.radioButton_emitidoPdf.Name = "radioButton_emitidoPdf";
-            this.radioButton_emitidoPdf.Size = new System.Drawing.Size(69, 15);
-            this.radioButton_emitidoPdf.TabIndex = 40;
-            this.radioButton_emitidoPdf.TabStop = true;
-            this.radioButton_emitidoPdf.Text = "Emitido";
-            this.radioButton_emitidoPdf.UseVisualStyleBackColor = true;
+            this.radioButton_emitido.Checked = true;
+            this.radioButton_emitido.Location = new System.Drawing.Point(18, 11);
+            this.radioButton_emitido.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton_emitido.Name = "radioButton_emitido";
+            this.radioButton_emitido.Size = new System.Drawing.Size(69, 15);
+            this.radioButton_emitido.TabIndex = 40;
+            this.radioButton_emitido.Text = "Emitido";
+            this.radioButton_emitido.UseVisualStyleBackColor = true;
+            this.radioButton_emitido.CheckedChanged += new System.EventHandler(this.radioButton_emitido_CheckedChanged);
             // 
-            // dataGridView1
+            // gridTrazas
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.gridTrazas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridTrazas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.fecha,
             this.descripcion});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 277);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 20;
-            this.dataGridView1.Size = new System.Drawing.Size(377, 256);
-            this.dataGridView1.TabIndex = 42;
+            this.gridTrazas.Location = new System.Drawing.Point(12, 277);
+            this.gridTrazas.Name = "gridTrazas";
+            this.gridTrazas.RowHeadersWidth = 20;
+            this.gridTrazas.Size = new System.Drawing.Size(377, 256);
+            this.gridTrazas.TabIndex = 42;
+            this.gridTrazas.TabStop = false;
             // 
             // fecha
             // 
@@ -270,19 +277,57 @@
             this.descripcion.HeaderText = "Descripcion";
             this.descripcion.Name = "descripcion";
             // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.linkLabel1);
+            this.groupBox4.Controls.Add(this.textDocumentacion);
+            this.groupBox4.Location = new System.Drawing.Point(12, 539);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(184, 84);
+            this.groupBox4.TabIndex = 43;
+            this.groupBox4.TabStop = false;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(9, -2);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(82, 13);
+            this.linkLabel1.TabIndex = 41;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Documentación";
+            // 
+            // textDocumentacion
+            // 
+            this.textDocumentacion.Location = new System.Drawing.Point(6, 19);
+            this.textDocumentacion.Multiline = true;
+            this.textDocumentacion.Name = "textDocumentacion";
+            this.textDocumentacion.Size = new System.Drawing.Size(172, 85);
+            this.textDocumentacion.TabIndex = 20;
+            this.textDocumentacion.TabStop = false;
+            this.textDocumentacion.Text = "Este endpoint permite generar y descargar archivo en formato PDF, correspondiente" +
+    " a DTE emitido en portal SimpleFactura.";
+            // 
             // ObtenerTrazasEmitidas_Recibidas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(398, 715);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.radioButton_recibidoPdf);
-            this.Controls.Add(this.radioButton_emitidoPdf);
+            this.ClientSize = new System.Drawing.Size(398, 632);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.gridTrazas);
+            this.Controls.Add(this.radioButton_recibido);
+            this.Controls.Add(this.radioButton_emitido);
             this.Controls.Add(this.obtener_trazas);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "ObtenerTrazasEmitidas_Recibidas";
-            this.Text = "ObtenerTrazasEmitidas_Recibidas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Obtener Trazas";
             this.Load += new System.EventHandler(this.ObtenerTrazasEmitidas_Recibidas_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
@@ -291,7 +336,9 @@
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.folio_oPDF)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTrazas)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -301,7 +348,7 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox textRutEmisor;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textRutContribuyente;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
@@ -312,10 +359,13 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button obtener_trazas;
-        private System.Windows.Forms.RadioButton radioButton_recibidoPdf;
-        private System.Windows.Forms.RadioButton radioButton_emitidoPdf;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RadioButton radioButton_recibido;
+        private System.Windows.Forms.RadioButton radioButton_emitido;
+        private System.Windows.Forms.DataGridView gridTrazas;
         private System.Windows.Forms.DataGridViewTextBoxColumn fecha;
         private System.Windows.Forms.DataGridViewTextBoxColumn descripcion;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TextBox textDocumentacion;
     }
 }
--- SimpleFacturaSDK-Demo/ObtenerTrazasEmitidas_Recibidas.cs
+++ SimpleFacturaSDK-Demo/ObtenerTrazasEmitidas_Recibidas.cs
@@ -1,14 +1,19 @@
 ﻿using SDKSimpleFactura;
+using SDKSimpleFactura.Models.Facturacion;
+using SDKSimpleFactura.Models.Response;
 using SimpleFacturaSDK_Demo.Helpers;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static SDKSimpleFactura.Enum.Ambiente;
 using static SDKSimpleFactura.Enum.TipoDTE;
 
 namespace SimpleFacturaSDK_Demo
@@ -27,7 +32,115 @@ namespace SimpleFacturaSDK_Demo
         private void ObtenerTrazasEmitidas_Recibidas_Load(object sender, EventArgs e)
         {
             EnumHelper.LlenarComboBoxConEnum<DTEType>(comboBox_codigoTipoDTE);
-
+            textRutEmisor.Text = _appSettings.Credenciales.RutEmisor;
+            comboBox_codigoTipoDTE.SelectedIndex = 3;
+            radioCertificacion.Checked = true;
         }
+        private void ChangeUI()
+        {
+            if (radioButton_emitido.Checked)
+            {
+                textRutContribuyente.Enabled = false;
+                folio_oPDF.Value = 8597;
+                textDocumentacion.Text = "Permite obtener las trazas del DTE emitido basado en el folio, tipo DTE, ambiente y el rut del emisor.";
+            }
+            else if (radioButton_recibido.Checked)
+            {
+                textRutContribuyente.Enabled = true;
+                folio_oPDF.Value = 2232;
+                textRutContribuyente.Text = "76269769-6";
+                textDocumentacion.Text = "Permite obtener las trazas del DTE recibido basado en el folio, tipo DTE, ambiente, rut emisor y el rut proveedor.";
+            }
+        }
+
+        private void radioButton_emitido_CheckedChanged(object sender, EventArgs e)
+        {
+            ChangeUI();
+        }
+
+        private void radioButton_recibido_CheckedChanged(object sender, EventArgs e)
+        {
+            ChangeUI();
+        }
+
+        private async void obtener_trazas_Click(object sender, EventArgs e)
+        {
+            Loading.ShowLoading(obtener_trazas);
+            try
+            {
+                AmbienteEnum ambienteSeleccionado;
+
+                if (radioCertificacion.Checked)
+                {
+                    ambienteSeleccionado = AmbienteEnum.Certificacion;
+                }
+                else if (radioProduccion.Checked)
+                {
+                    ambienteSeleccionado = AmbienteEnum.Produccion;
+                }
+                else
+                {
+                    // Si no se selecciona ningún ambiente, mostrar un mensaje
+                    MessageBox.Show("Por favor, selecciona un ambiente (Certificación o Producción).", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                Response<List<TrazasEnt>> response;
+                var request = new SolicitudDte
+                {
+                    Credenciales = new SDKSimpleFactura.Models.Facturacion.Credenciales(), // Inicializamos Credenciales
+                    DteReferenciadoExterno = new DteReferenciadoExterno() // Inicializamos DteReferenciadoExterno
+                };
+                if (radioButton_emitido.Checked)
+                {
+                    
+                    request.Credenciales.RutEmisor = textRutEmisor.Text;
+                    request.DteReferenciadoExterno.Folio = (int)folio_oPDF.Value;
+                    request.DteReferenciadoExterno.CodigoTipoDte = (int)EnumHelper.ObtenerValorSeleccionado<DTEType>(comboBox_codigoTipoDTE);
+                    request.DteReferenciadoExterno.Ambiente = (int)ambienteSeleccionado;
+                    response = await cliente.Facturacion.GetTrazasEmitidosAsync(request);
+                }
+                else
+                {
+                    request.Credenciales.RutEmisor = textRutEmisor.Text;
+                    request.Credenciales.RutContribuyente = textRutContribuyente.Text;
+                    request.DteReferenciadoExterno.Folio = (int)folio_oPDF.Value;
+                    request.DteReferenciadoExterno.CodigoTipoDte = (int)EnumHelper.ObtenerValorSeleccionado<DTEType>(comboBox_codigoTipoDTE);
+                    request.DteReferenciadoExterno.Ambiente = (int)ambienteSeleccionado;
+                    response = await cliente.Proveedores.GetTrazasRecibidosAsync(request);
+                }
+                if (response.Status == 400 || response.Status == 500)
+                {
+                    MessageBox.Show(response.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    if (response.Data == null) 
+                    {
+                        MessageBox.Show("No se encontraron trazas","Éxito",MessageBoxButtons.YesNo,MessageBoxIcon.Information);
+                        Loading.HideLoading(obtener_trazas);
+                        return;
+                    }
+                    gridTrazas.DataSource = null;
+                    gridTrazas.DataSource = response.Data;
+                    gridTrazas.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                    var fechaCol = gridTrazas.Columns["fecha"];
+                    if (fechaCol != null)
+                    {
+                        fechaCol.DefaultCellStyle.Format = "dd/MM/yyyy";
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocurrió un error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                Loading.HideLoading(obtener_trazas);
+            }
+        }
+
     }
+
 }
